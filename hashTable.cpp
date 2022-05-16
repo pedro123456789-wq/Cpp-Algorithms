ed12b2cb732de46c3da82ec2fb5af7bc5a54da41
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-05-16 13:43:50 +0100

    Update hashTable.cpp

--- hashTable.cpp
+++ hashTable.cpp
@@ -2,25 +2,62 @@
 using namespace std;
 
 
-struct hashTableItem{
-  char* key;
-  int value;
-};
 
-
-class hashTable{
-  private:
+class HashTable{
+private:
+  int* valuesArray;
+  char** keysArray;
+  int tableSize;
   
+public:
+  hashTable(int size){
+    tableSize = size;
+    valuesArray = (int*)malloc(size * sizeof(int));
+    keysArray = (char**)malloc(size * sizeof((char*)malloc(size * sizeof(char))));
+  }
+  
+  // algorithm to convert string into index
   int hashFunction(char* str){
-    unsigned long hash = 5381;
-    unsigned int size = str.length();
-    unsigned int i = 0;
+    long hash = 123;
+    int index = 0;
+    char currentChar = str[index];
     
-    for (i = 0; i < size; i++) {
-        hash = ((hash << 5) + hash) + (str[i]); /* hash * 33 + c */
+    while (currentChar) {
+        hash = ((hash << 5) + hash) + (currentChar);
+        currentChar = str[index ++];
+    } 
+    
+    // make hash positive and within the bounds of the array 
+    return abs(hash) % tableSize;
+  }
+  
+  // add new element to correct index in array
+  void put(char* key, int value){
+    int index = hashFunction(key);
+    
+    if (keysArray[index] == 0){
+      valuesArray[index] = value;
+      keysArray[index] = key;
+    }else{
+      if (keysArray[index] == key){
+        cout << "Key already in use" << endl;
+      }else{
+        cout << "Collision" << endl;
+        // TODO: Add collision handling
+      }
     }
-
-    return hash;
+  }
+  
+  // get element from hash table 
+  int get(char* key){
+    int index = hashFunction(key);
+    
+    if (keysArray[index] != 0){
+      return valuesArray[index];
+    }else{
+      cout << "Invalid Key" << endl;
+      return -1;
+    }
   }
 };
 
@@ -28,6 +65,11 @@ class hashTable{
 
 int main() 
 {
-    cout << "Hello, World!";
-    return 0;
+  hashTable table = hashTable(60);
+  table.put("t1", 2);
+  table.put("t4", 5);
+  table.put("t4", 7);
+  
+  cout << table.get("t1") << endl;
+  cout << table.get("t4") << endl;
 }
