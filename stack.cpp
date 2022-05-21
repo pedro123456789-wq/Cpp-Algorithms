148f8d202368b99856fe8aa774b0ec6bb9689bab
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-05-21 20:09:46 +0100

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+	int* array;
+	int top = 0;
+	int maxSize;
+
+public:
+	Stack(int arraySize) {
+		maxSize = arraySize;
+		array = (int*) malloc(arraySize * sizeof(int));
+	}
+
+	void push(int item) {
+		if (!isFull()) {
+			array[top] = item;
+			top++;
+		}
+	}
+
+	int pop() {
+		if (!isEmpty()) {
+			top--;
+			return array[top];
+		} else {
+			return -1;
+		}
+	}
+
+	bool isFull() {
+		return (top == maxSize);
+	}
+
+	bool isEmpty() {
+		return (top == 0);
+	}
+
+	void showStack() {
+		cout << "[";
+
+		for (int i = 0; i < top; i++) {
+			cout << array[i] << ",";
+		}
+
+		cout << "]" << endl;
+	}
+};
